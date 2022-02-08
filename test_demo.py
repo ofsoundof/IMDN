fb223ee439ce349f714d19c390cca7a4d4f4d4c1
Yawei Li <yaweili@fb.com>
2022-02-08 11:59:41 +0100

    Initial commit

--- test_demo.py
+++ test_demo.py
@@ -91,7 +91,7 @@ def main():
         # --------------------------------
         img_E = util.tensor2uint(img_E)
 
-        # util.imsave(img_E, os.path.join(E_folder, img_name+ext))
+        util.imsave(img_E, os.path.join(E_folder, img_name+ext))
     ave_runtime = sum(test_results['runtime']) / len(test_results['runtime']) / 1000.0
     logger.info('------> Average runtime of ({}) is : {:.6f} seconds'.format(L_folder, ave_runtime))
 
